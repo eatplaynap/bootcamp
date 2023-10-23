25cbf94fdaf5f3a9e2b82d6873a1edabdef4eafc
masuyama13 <masterxwebm13@gmail.com>
2023-10-23 21:38:39 +0900

    不可逆なマイグレーションをリバーシブルに書き換え

--- db/migrate/20231013073200_add_correct_default_on_statements.rb
+++ db/migrate/20231013073200_add_correct_default_on_statements.rb
@@ -1,5 +1,5 @@
 class AddCorrectDefaultOnStatements < ActiveRecord::Migration[6.1]
   def change
-    change_column_default :statements, :correct?, false
+    change_column_default :statements, :correct?, from: nil, to: false
   end
 end
--- db/migrate/20231021045431_change_statements_is_correct_not_null_and_default.rb
+++ db/migrate/20231021045431_change_statements_is_correct_not_null_and_default.rb
@@ -1,6 +1,6 @@
 class ChangeStatementsIsCorrectNotNullAndDefault < ActiveRecord::Migration[6.1]
   def change
     change_column_null :statements, :is_correct, false
-    change_column_default :statements, :is_correct, nil
+    change_column_default :statements, :is_correct, from: false, to: nil
   end
 end
